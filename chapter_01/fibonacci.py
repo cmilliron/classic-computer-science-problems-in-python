4949da7e7609b4870b1c0c0e281fc42cf61bba5c
Cody Milliron <codymilliron.dev@gmail.com>
2025-01-26 16:39:24 -0500

    Fibonacci numbers

--- chapter_01/fibonacci.py
+++ chapter_01/fibonacci.py
@@ -29,8 +29,36 @@ def fib4(n:int) -> int:
     return fib4(n-1) + fib4(n-2)
 
 
+# Using iteration and tuple unpacking
+def  fib5(n:int) -> int:
+    if n ==0 :
+        return n
+    last: int = 0
+    next: int = 1
+    for _ in range(1, n):
+        last, next = next, last + next
+    return next
+
+
+# Using a Generator
+from typing import Generator
+
+def fib6(n: int) -> Generator[int, None, None]:
+    yield 0 # special case
+    if n > 0: yield 1
+    last: int = 0  # initally set to fib(0)
+    next: int = 1  # initially set to fib(1)
+    for _ in range(1, n):
+        last, next = next, last + next
+        yield next # main generation step 
+
 if __name__ == "__main__":
     # print(fib_recursive(7))
     print(fib3(50))
 
     print(fib4(50))
+
+    print(fib5(50))
+
+    for i in fib6(50):
+        print(i)
